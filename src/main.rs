542a0d2110eb27772ff8a0270672b312273ae1c9
calebmer <calebmeredith8@gmail.com>
2016-03-19 17:47:18 -0400

    feat(server): add server

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,48 @@
-static GRAPHIQL_HTML: &'static str = include_str!("../graphiql.html");
+extern crate ansi_term;
+#[macro_use(crate_version)]
+extern crate clap;
+extern crate hyper;
+extern crate iron;
+extern crate logger;
+extern crate time;
+
+mod server;
+
+use ansi_term::Style;
+use clap::{App, AppSettings, Arg};
+use iron::{Iron, Chain};
+use logger::Logger;
+use server::Server;
 
 fn main() {
-  println!("{}", GRAPHIQL_HTML);
+  let matches = {
+    App::new("Ardite GraphQL Server")
+    .version(crate_version!())
+    .author("Caleb Meredith <calebmeredith8@gmail.com>")
+    .about("An Ardite service for a GraphQL server.")
+    .version_short("v")
+    .setting(AppSettings::UnifiedHelpMessage)
+    .args(&[
+      Arg::with_name("hostname").long("hostname").short("n").takes_value(true).value_name("STRING").help("The host name that the server will listen on"),
+      Arg::with_name("port").long("port").short("p").takes_value(true).value_name("PORT").help("The port that the server will listen on"),
+      Arg::with_name("enable_graphiql").long("graphiql").short("i").help("Enable the serving of a GraphiQL interface to your GraphQL server")
+    ])
+    .get_matches()
+  };
+
+  let hostname = matches.value_of("hostname").unwrap_or("localhost");
+  let port = matches.value_of("port").unwrap_or("3002").parse::<u16>().unwrap();
+
+  let server = Server {
+    enable_graphiql: matches.is_present("enable_graphiql")
+  };
+
+  let mut chain = Chain::new(server);
+  let (logger_before, logger_after) = Logger::new(None);
+
+  chain.link_before(logger_before);
+  chain.link_after(logger_after);
+
+  println!("GraphQL server listening on {}", Style::new().underline().paint(format!("http://{}:{}", hostname, port)));
+  Iron::new(chain).http((hostname, port)).unwrap();
 }
---
+++ src/server.rs
@@ -0,0 +1,57 @@
+use iron::prelude::*;
+use iron::headers::{Date, HttpDate, CacheControl, CacheDirective, ContentType};
+use iron::mime::{Mime, TopLevel, SubLevel, Attr, Value};
+use iron::modifiers::Header;
+use iron::status::Status;
+use iron::Handler;
+use time;
+
+static GRAPHQL_ENDPOINT: &'static str = "graphql";
+static GRAPHIQL_ENDPOINT: &'static str = "graphiql";
+static GRAPHIQL_HTML: &'static str = include_str!("../graphiql.html");
+
+pub struct Server {
+  pub enable_graphiql: bool
+}
+
+impl Handler for Server {
+  fn handle(&self, req: &mut Request) -> IronResult<Response> {
+    let mut res = Response::new();
+    let mut path = req.url.path.clone();
+
+    res.set_mut(Header(Date(HttpDate(time::now()))));
+    res.set_mut(Header(CacheControl(vec![CacheDirective::NoCache, CacheDirective::Private])));
+
+    if path.len() == 1 {
+      // All of our endpoints are only one key.
+      let endpoint = path.pop().unwrap();
+
+      if endpoint == GRAPHQL_ENDPOINT {
+        // If this is our GraphQL endpoint, let’s run the query.
+
+        res.set_mut(Header(ContentType(Mime(TopLevel::Application, SubLevel::Json, vec![(Attr::Charset, Value::Utf8)]))));
+        res.set_mut(Status::InternalServerError);
+        res.set_mut("{}");
+
+        return Ok(res);
+      }
+
+      if self.enable_graphiql && endpoint == GRAPHIQL_ENDPOINT {
+        // If the developer has enabled GraphiQL and this is GraphiQL’s
+        // endpoint, return the page.
+
+        res.set_mut(Header(ContentType(Mime(TopLevel::Text, SubLevel::Html, vec![(Attr::Charset, Value::Utf8)]))));
+        res.set_mut(Status::Ok);
+        res.set_mut(GRAPHIQL_HTML);
+
+        return Ok(res);
+      }
+    }
+
+    res.set_mut(Header(ContentType(Mime(TopLevel::Text, SubLevel::Plain, vec![(Attr::Charset, Value::Utf8)]))));
+    res.set_mut(Status::NotFound);
+    res.set_mut("Not Found");
+
+    Ok(res)
+  }
+}
