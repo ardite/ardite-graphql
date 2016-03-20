31c4a144821317ec7635412cbf6c616dd7c7ec80
calebmer <calebmeredith8@gmail.com>
2016-03-20 14:00:34 -0400

    style(main): style changes

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ use ansi_term::Style;
 use clap::{App, AppSettings, Arg};
 use iron::{Iron, Chain};
 use logger::Logger;
+
 use server::Server;
 
 fn main() {
@@ -23,26 +24,31 @@ fn main() {
     .version_short("v")
     .setting(AppSettings::UnifiedHelpMessage)
     .args(&[
-      Arg::with_name("hostname").long("hostname").short("n").takes_value(true).value_name("STRING").help("The host name that the server will listen on"),
-      Arg::with_name("port").long("port").short("p").takes_value(true).value_name("PORT").help("The port that the server will listen on"),
+      Arg::with_name("hostname").long("hostname").short("n").takes_value(true).default_value("localhost").value_name("STRING").help("The host name that the server will listen on"),
+      Arg::with_name("port").long("port").short("p").takes_value(true).deafult_value("3002").value_name("PORT").help("The port that the server will listen on"),
       Arg::with_name("enable_graphiql").long("graphiql").short("i").help("Enable the serving of a GraphiQL interface to your GraphQL server")
     ])
     .get_matches()
   };
 
-  let hostname = matches.value_of("hostname").unwrap_or("localhost");
-  let port = matches.value_of("port").unwrap_or("3002").parse::<u16>().unwrap();
+  let hostname = matches.value_of("hostname").unwrap();
+  let port = matches.value_of("port").unwrap().parse::<u16>().unwrap();
 
   let server = Server {
     enable_graphiql: matches.is_present("enable_graphiql")
   };
 
   let mut chain = Chain::new(server);
+
   let (logger_before, logger_after) = Logger::new(None);
 
   chain.link_before(logger_before);
   chain.link_after(logger_after);
 
-  println!("GraphQL server listening on {}", Style::new().underline().paint(format!("http://{}:{}", hostname, port)));
+  println!(
+    "GraphQL server listening on {}",
+    Style::new().underline().paint(format!("http://{}:{}", hostname, port))
+  );
+
   Iron::new(chain).http((hostname, port)).unwrap();
 }
